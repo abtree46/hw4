2e13537c423782dd05379aed2a4dcab77f67468e
Ashley Bleibtrey <ashleybleibtrey1@gmail.com>
2020-06-23 23:56:28 -0500

    all i've got

---
+++ questions.js
@@ -0,0 +1,12 @@
+var questions = [
+	{
+		title: "Which of these defines the style of a webpage?",
+		choices: ["css", "H1", "script", "trees"],
+		answer: "css"
+	},
+	{
+		title: "Which of these is an hmtl element?",
+		choices: ["text-align", "color", "div", "Puerto Rico"],
+		answer: "div"
+	}
+]
